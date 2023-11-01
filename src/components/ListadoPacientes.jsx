d8281237df3aeddf6650395afbed47849180d3fd
facundojperalta <lavezzi_7_94@hotmail.com>
2023-11-01 18:56:15 -0300

    Se agregar archivos base del proyecto

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+
+
+function ListadoPacientes() {
+  return (
+    <div className="md:w-1/2 lg:w-3/5">
+      <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
+    </div>
+  )
+}
+
+export default ListadoPacientes
