0fba480e22b24db74dfbb17dfc506c5a7cf83ba1
Facundo Javier Peralta <76714825+facundojperalta@users.noreply.github.com>
2023-11-30 20:15:59 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <>
-            <img className="mx-auto py-1" src="https://cdn.discordapp.com/attachments/627758835540557834/1179923174771662939/logo_1.png?ex=657b8c16&is=65691716&hm=946708d9ce866c7145b5d461f9a09d51162f6a865d1ee69487ebfc347db7d471&" alt="Logo Veterinaria" />
+            <img className="mx-auto py-1" src="https://shorturl.at/hjB23" alt="Logo Veterinaria" />
             <h1 className="font-black text-2xl text-center md:w-2/3 mx-auto">
                 Seguimiento Pacientes {""}
                 <span className="text-pink-600">Veterinaria</span>
